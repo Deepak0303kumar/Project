5e0017544a041d5f34087902383c1817f028cdb9
Deepak0303kumar <mishradeepak1137@gmail.com>
2026-05-24 09:37:06 +0530

    loop1

--- condition.js
+++ condition.js
@@ -1,17 +1,10 @@
-const age=32
-if(age<18)
-{
-    console.log("Not approved")
+const age = 32;
+if (age < 18) {
+  console.log("Not approved");
+} else if (age >= 18 && age <= 60) {
+  console.log("License Approved");
+} else if (age >= 61 && age <= 150) {
+  console.log("License will not be given");
+} else {
+  console.log("You will be dead");
 }
-else if(age>=18 && age<=60)
-{
-    console.log("License Approved")
-}
-else if(age>=61 && age<=150)
-{
-    console.log("License will not be given")
-}
-   else
-   {
-    console.log("You will be dead")
-   }

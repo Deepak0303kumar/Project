844764260b3080583f31cad2f72b05afb5b8e1de
Deepak0303kumar <mishradeepak1137@gmail.com>
2026-05-24 09:21:35 +0530

    Value assisgned

---
+++ condition.js
@@ -0,0 +1,17 @@
+const age=32
+if(age<18)
+{
+    console.log("Not approved")
+}
+else if(age>=18 && age<=60)
+{
+    console.log("License Approved")
+}
+else if(age>=61 && age<=150)
+{
+    console.log("License will not be given")
+}
+   else
+   {
+    console.log("You will be dead")
+   }

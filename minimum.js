908e555dcaa757ec1c115f01a9f0b2f994def74e
Deepak0303kumar <mishradeepak1137@gmail.com>
2026-05-29 09:08:35 +0530

    array1

--- minimum.js
+++ minimum.js
@@ -1,7 +1,6 @@
-const arr =[23,21,55,29,90,65,87];
-let max=0;
-  for(let i=0;i<arr.length;i++){
-     if(max>arr[i])
-        max=arr[i];
-  }
-  console.log("Maximum number in the array: ",min);
+const arr = [23, 21, 55, 29, 90, 65, 87];
+let min = arr[0];
+for (let i = 0; i < arr.length; i++) {
+  if (min > arr[i]) min = arr[i];
+}
+console.log("Minimum number in the array: ", min);
